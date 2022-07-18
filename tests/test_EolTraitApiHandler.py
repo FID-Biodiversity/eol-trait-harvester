c798f9e31fe259dc84350c8f6a8d05308d6083b5
Adrian Pachzelt <a.pachzelt@ub.uni-frankfurt.de>
2022-07-18 16:12:55 +0200

    Fixed EolTraitApiHandler implementation

--- tests/test_EolTraitApiHandler.py
+++ tests/test_EolTraitApiHandler.py
@@ -1,9 +1,24 @@
+import json
+from copy import copy
+from dataclasses import dataclass
+from unittest.mock import Mock
+
 import pytest
+import requests
 
 from eol.handlers import EolTraitApiHandler
 
 
-class TestEolTraitApiHandler:
+def internet_connection_available():
+    try:
+        requests.get('https://www.duckduckgo.com/')
+        return True
+    except requests.exceptions.ConnectionError:
+        return False
+
+
+@pytest.mark.skipif(not internet_connection_available(), reason='No internet connection available')
+class TestEolTraitApiHandlerWebCalls:
     def test_get_next(self, eol_trait_api_handler):
         cypher_api = eol_trait_api_handler.iterate()
         item1 = next(cypher_api)
@@ -11,24 +26,84 @@ class TestEolTraitApiHandler:
         item2 = next(cypher_api)
         assert item2 is not None and len(item2) and item2 != item1
 
-    def test_get_data_from_cypher_api(self, eol_trait_api_handler, cypher_query_string_for_habitat_data):
-        response = eol_trait_api_handler.get_data_from_cypher_api(cypher_query_string_for_habitat_data)
+    def test_get_data_from_cypher_api(
+        self, eol_trait_api_handler, cypher_query_string_for_habitat_data
+    ):
+        response = eol_trait_api_handler.get_data_from_cypher_api(
+            cypher_query_string_for_habitat_data
+        )
         assert len(response) == 5
 
-    @pytest.fixture
-    def eol_trait_api_handler(self, eol_api_credentials):
-        return EolTraitApiHandler(api_credentials=eol_api_credentials)
 
-    @pytest.fixture
-    def cypher_query_string_for_habitat_data(self):
-        return """MATCH (t:Trait)<-[:trait]-(p:Page),
-            (t)-[:supplier]->(r:Resource),
-            (t)-[:predicate]->(pred:Term)
-            WHERE pred.uri = "http://purl.obolibrary.org/obo/RO_0002303"
-            OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
-            OPTIONAL MATCH (t)-[:normal_units_term]->(units:Term)
-            RETURN r.resource_id, t.eol_pk, t.resource_ok, t.source,
-            p.page_id, t.scientific_name, pred.uri, pred.name,
-            t.object_page_id, obj.uri, obj.name, t.normal_measurement,
-            units.uri, units.name, t.normal_units, t.literal
-            LIMIT 5"""
+class TestEolTraitApiHandlerUnittests:
+    def test_paginate_cypher_api(self, eol_trait_api_handler):
+        eol_trait_api_handler.read_api_with_parameters = Mock()
+        eol_trait_api_handler.read_api_with_parameters.side_effect = (
+            generate_mock_responses(number_of_responses=3)
+        )
+
+        list(
+            eol_trait_api_handler.paginate_cypher_api(
+                "MATCH (trait:Trait) RETURN trait LIMIT 100;"
+            )
+        )
+
+        expected_queries = [
+            eol_trait_api_handler.compose_cypher_url(
+                "MATCH (trait:Trait) RETURN trait SKIP 0 LIMIT 100"
+            ),
+            eol_trait_api_handler.compose_cypher_url(
+                "MATCH (trait:Trait) RETURN trait SKIP 100 LIMIT 100"
+            ),
+            eol_trait_api_handler.compose_cypher_url(
+                "MATCH (trait:Trait) RETURN trait SKIP 200 LIMIT 100"
+            ),
+        ]
+
+        for query in expected_queries:
+            eol_trait_api_handler.read_api_with_parameters.assert_any_call(query)
+
+
+@pytest.fixture
+def eol_trait_api_handler(eol_api_credentials):
+    return EolTraitApiHandler(api_credentials=eol_api_credentials)
+
+
+@pytest.fixture
+def cypher_query_string_for_habitat_data():
+    return """MATCH (t:Trait)<-[:trait]-(p:Page),
+        (t)-[:supplier]->(r:Resource),
+        (t)-[:predicate]->(pred:Term)
+        WHERE pred.uri = "http://purl.obolibrary.org/obo/RO_0002303"
+        OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+        OPTIONAL MATCH (t)-[:normal_units_term]->(units:Term)
+        RETURN r.resource_id, t.eol_pk, t.resource_ok, t.source,
+        p.page_id, t.scientific_name, pred.uri, pred.name,
+        t.object_page_id, obj.uri, obj.name, t.normal_measurement,
+        units.uri, units.name, t.normal_units, t.literal
+        LIMIT 5"""
+
+
+@dataclass
+class MockResponse:
+    status_code: int
+    text: str
+
+
+def generate_mock_responses(number_of_responses=100):
+    mock_data = {
+        "columns": ["p.page_id", "t.scientific_name"],
+        "data": [(12345, "Fagus testus")],
+    }
+
+    last_element = copy(mock_data)
+    last_element["data"] = []
+
+    responses = [
+        MockResponse(text=json.dumps(mock_data), status_code=200)
+        for _ in range(0, number_of_responses)
+    ]
+
+    responses[-1] = MockResponse(text=json.dumps(last_element), status_code=200)
+
+    return responses
--- tests/test_EolTraitCsvHandler.py
+++ tests/test_EolTraitCsvHandler.py
@@ -8,11 +8,14 @@ class TestEolTraitCsvHandler:
         item = next(eol_traits_csv_handler.iterate())
         assert isinstance(item, dict)
 
-    def test_get_data_filtered_by_value(self, eol_traits_csv_handler):
-        data = eol_traits_csv_handler.get_data_filtered_by_value(key='page_id', value=45258442)
-        assert data['eol_pk'] == 'R533-PK221522710'
+    def test_iterate_data_by_key(self, eol_traits_csv_handler):
+
+        data = next(eol_traits_csv_handler.iterate_data_by_key(
+            key="page_id", value=45258442
+        ))
+        assert data["eol_pk"] == "R533-PK221522710"
 
     @pytest.fixture
     def eol_traits_csv_handler(self, resource_directory):
-        csv_file_path_string = resource_directory / 'test_eol_traits.csv'
+        csv_file_path_string = resource_directory / "test_eol_traits.csv"
         return EolTraitCsvHandler(csv_file_path_string)
